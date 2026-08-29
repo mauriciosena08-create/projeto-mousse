e5f8d353d065d6b980114f4b7a214a1e74b6ac53
Kevin Freitas <kevinfreitasv80@gmail.com>
2026-08-29 18:55:45 -0300

    primeiro commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Gothic_A1 } from "next/font/google";
+import "./globals.css";
+
+const GothicA1 = Gothic_A1({
+  variable: "--font-gothic-a1",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
+
+export const metadata: Metadata = {
+  title: "Infodoces — O mais gostosos do IF",
+  description: "Bateu aquela fome entre uma aula e outra? A Infodoces chegou pra salvar! 😎",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="en"
+      className={`${GothicA1.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
